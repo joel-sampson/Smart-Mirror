15243892eb0051a45bb708f1e463559cfc35018a
joel-sampson <joel-sampson@internode.on.net>
2022-01-23 13:37:50 +1030

    Prevent getting dependencies as root to make installation less intrusive

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import os
 import sys
 
 # Must be ran as root or as sudo
-if os.getuid() != 0:
-    print("ERROR: Need to run as root")
+if os.getuid() == 0:
+    print("ERROR: Do not run as root as root")
     sys.exit(1)
 
 # Install the requirements if the system does not have it installed
-print("INFO: Checking and installing requirements")
-os.system("! dpkg -S python3-tk && apt-get -y install python3-tk")
+# print("INFO: Checking and installing requirements")
+# os.system("! dpkg -S python3-tk && apt-get -y install python3-tk")
 
 # Generate the requirements from the file for old instructions
 print("INFO: Generating the requirements from requirements.txt")
@@ -21,4 +21,4 @@ for line in open("requirements.txt", "r"):
 
 # Run pip
 print("INFO: installing pip packages")
-os.system("pip install -r requirements.txt")
+os.system("python3 -m pip install --user -r requirements.txt")
