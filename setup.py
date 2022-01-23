6f9b970dc3b8fbf464f3d73ae5f9967d5519600c
joel-sampson <joel-sampson@internode.on.net>
2022-01-23 13:15:14 +1030

    run pip via shell instead of using setuptools (because of error)

--- setup.py
+++ setup.py
@@ -2,31 +2,23 @@
 
 import os
 import sys
-from setuptools import setup, find_packages
 
 # Must be ran as root or as sudo
 if os.getuid() != 0:
-    print('ERROR: Need to run as root')
+    print("ERROR: Need to run as root")
     sys.exit(1)
 
 # Install the requirements if the system does not have it installed
-print('INFO: Checking and installing requirements')
-os.system('! dpkg -S python3-tk && apt-get -y install python3-tk')
+print("INFO: Checking and installing requirements")
+os.system("! dpkg -S python3-tk && apt-get -y install python3-tk")
 
 # Generate the requirements from the file for old instructions
-print('INFO: Generating the requirements from requirements.txt')
+print("INFO: Generating the requirements from requirements.txt")
 packages = []
-for line in open('requirements.txt', 'r'):
-    if not line.startswith('#'):
+for line in open("requirements.txt", "r"):
+    if not line.startswith("#"):
         packages.append(line.strip())
 
-# Run setuptools for pip
-setup(
-    name='smartmirror',
-    version='1.0.0',
-    description='Raspberry powered mirror which can display news, weather, calendar events',
-    author='HackerHouse',
-    url='https://github.com/HackerHouseYT/Smart-Mirror',
-    install_requires=packages,
-    packages=find_packages(),
-)
+# Run pip
+print("INFO: installing pip packages")
+os.system("pip install -r requirements.txt")
